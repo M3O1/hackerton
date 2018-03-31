a278e0a42ad2eb36bd32feb4f9cedfdad613986b
sangjaekang <sangjaekang48@gmail.com>
2018-03-31 16:00:56 +0900

    제목 변경

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+from datetime import datetime
+import numpy as np
 import json
 
 app = Flask(__name__,static_url_path="" ,static_folder="./dist", template_folder="./templates")
@@ -7,12 +9,11 @@ app = Flask(__name__,static_url_path="" ,static_folder="./dist", template_folder
 def index():
     return render_template("index.html")
 
-
 @app.route("/single_point")
 def get_single_point():
     lat = request.args.get('lat', default=1.,type=float)
     lng = request.args.get('lng', default=1.,type=float)
-    print(lat, lng)
+
     data = {'lat':40.7,"lng":-74.03}
     response = app.response_class(
             response=json.dumps(data),
@@ -21,20 +22,19 @@ def get_single_point():
             )
     return response
 
+
 @app.route("/multi_point")
 def get_multi_point():
     lat = request.args.get('lat', default=1.,type=float)
     lng = request.args.get('lng', default=1.,type=float)
 
-    data = [{'lat':40.7,"lng":-74.03},{'lat':40.4,"lng":-74.53},{'lat':40.65,"lng":-73.53}]
+    data = [{'lat':40.71,"lng":-74.03},{'lat':40.69,"lng":-74.04},{'lat':40.69,"lng":-74.02}]
     response = app.response_class(
             response=json.dumps(data),
             status=200,
             mimetype='application/json'
             )
     return response
-
-
 
 if __name__ == "__main__":
     app.run()
